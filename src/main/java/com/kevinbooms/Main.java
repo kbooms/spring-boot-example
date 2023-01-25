bec2eb91a7c03feb0b4aa26bc3d290e05268e5bd
Kevin Booms <mrkbooms@gmail.com>
2023-01-25 18:32:44 -0500

    Setting up Greeting as Object

--- src/main/java/com/kevinbooms/Main.java
+++ src/main/java/com/kevinbooms/Main.java
@@ -1,9 +1,14 @@
 package com.kevinbooms;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -12,8 +17,20 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/")
-    public String greet() {
-        return "Hello Spring!";
+    // this method will override the Whitelabel error page
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        GreetResponse response = new GreetResponse(
+                "Go ahead. Make my day.",
+                List.of("Java", "Python", "JavaScript"),
+                new Person("Kevin")
+        );
+        return response;
     }
+
+    record Person (String name) {}
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person){}; // records are classes that allow us to achieve immutability
 }
