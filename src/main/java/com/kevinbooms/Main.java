e3a265f51961af618d42deb9b01f6dbd8b106a8a
Kevin Booms <mrkbooms@gmail.com>
2023-02-01 23:52:46 -0500

    We have GET

--- src/main/java/com/kevinbooms/Main.java
+++ src/main/java/com/kevinbooms/Main.java
@@ -3,39 +3,28 @@ package com.kevinbooms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/")
-    public String hello() {
-        return "You've got this. Just try to do your best.";
-    }
-    // this method will override the Whitelabel error page. It is called by default because we have declared this
-    // application as a Rest Controller with the @RestController annotation
-
-
-    // this code below is a JSON test. I'll leave it in for now.
-    @GetMapping("/json_test")
-    public GreetResponse greet() {
-        GreetResponse response = new GreetResponse(
-                "Go ahead. Make my day.",
-                List.of("Java", "Python", "JavaScript"),
-                new Person("Kevin")
-        );
-        return response;
+    @GetMapping()
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
 
-    record Person (String name) {}
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person){}; // records are classes that allow us to achieve immutability
 }
